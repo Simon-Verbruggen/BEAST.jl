cf70bcbfe6bd9b5d965769b6f568bf53e07fbe95
Simon Verbruggen <Verbruggen.simon@gmail.com>
2025-12-22 11:12:56 +0100

    Cleanup OSRC

--- src/operators/OSRC.jl
+++ src/operators/OSRC.jl
@@ -34,8 +34,8 @@ struct OSRC_op <: Operator
     Bj::Vector{ComplexF64}
 end
 
-# more details of this OSRC operator are given in : TODO add
-imag_conv = -im     # Geuzaine paper uses other time-harmonic convention -> swap to match BEAST time-harmonic
+# more details of this OSRC operator are given in :  <a href="https://arxiv.org/abs/2111.10761" target='new'> An OSRC Preconditioner for the EFIE (Betcke et al. (2021))</a>.
+imag_conv = -im     # The paper uses mathematical time-harmonic convention ``e^{-i \omega t}`` -> swap to match BEAST time-harmonic convention ``e^{i \omega t}``.
 function OSRC_op(wavenumber::Float64, Np::Int, θ_p::Float64, curvature::Float64)
     # get the real and rotated pade coefficients
     aj = ComplexF64[]
@@ -56,7 +56,6 @@ function OSRC_op(wavenumber::Float64, Np::Int, θ_p::Float64, curvature::Float64
     return OSRC_op(wavenumber, Np, θ_p, curvature, aj, bj, Aj, Bj)
 end
 
-# TODO: maybe better implementation type
 function scalartype(op::OSRC_op)
     T = scalartype(op.wavenumber)
     return Complex{T}
@@ -86,9 +85,6 @@ function rotated_implicit_pade(get_Π, OSRC)
     return get_R0(OSRC)*I - sum(OSRC.Aj[j]*I/(OSRC.Bj[j]*(get_Π(j, OSRC))) for j in 1:OSRC.Np)
 end
 
-# inverse Pade
-
-# converting pade rational to polynomial object
 
 # get polynomial representation of pade and get zeros and poles
 
@@ -167,7 +163,140 @@ function MtE_damping(;wavenumber=nothing, curvature=nothing)
     return 0.39*wavenumber^(1/3)*curvature^(2/3)
 end
 
-# Next, the MtE operator is assembled as a linear map. This is implemented according to the discretization in the paper <a href="https://arxiv.org/abs/2111.10761" target='new'> An OSRC Preconditioner for the EFIE (Betcke et al. (2021))</a>.
+# Next, the MtE operator is assembled as a linear map. This is implemented according to the discretization in the paper.
+
+function assemble(op::OSRC_op,X::Space,Y::Space; quadstrat=defaultquadstrat)
+    R_0 = get_R0(op)
+
+    ϵ = MtE_damping(op)
+    κ = op.wavenumber
+    κ_ϵ = κ + imag_conv*ϵ
+
+    #create auxilary basis functions
+    L0_int = BEAST.lagrangec0d1(X.geo)
+    grad_L0_int = BEAST.gradient(L0_int)
+    # Define the relevant function spaces
+    curl_X = BEAST.curl(X)
+    curl_Y = BEAST.curl(Y)
+
+    N_L0 = numfunctions(L0_int)
+    N_X = numfunctions(X)
+    N_Y = numfunctions(Y)
+
+    # Assemble the submatrices of the blockmatrix of the system
+    Id = BEAST.Identity();
+    G = assemble(Id, X, Y)
+    N_ϵ = (1/κ_ϵ)^2 * assemble(Id, curl_X, curl_Y)
+    K_ϵ = κ_ϵ^2 * assemble(Id, L0_int, L0_int)
+    L = assemble(Id, X, grad_L0_int)
+    L_transpose = assemble(Id, grad_L0_int, Y)
+
+    # construct the sparse system matrix and invert
+    function create_j_phi_matrix17(j)
+        B_j = op.Bj[j]
+        # blockmatrix of sparse matrices
+        AXY = [G-B_j*N_ϵ       B_j*L
+                L_transpose     K_ϵ]
+        SXY = BEAST.lu(AXY)
+        Sliced_SXY = SlicedLinearMap(SXY, 1:N_X, 1:N_Y)
+        P = Sliced_SXY.P
+        Q = Sliced_SXY.Q
+        SXY_sliced = P*SXY*Q
+        return SXY_sliced
+    end
+
+    sum_Π_inv_matrix = sum(op.Aj[j]/op.Bj[j] * create_j_phi_matrix17(j) for j in 1:op.Np)
+    G_N_ϵ_inv = BEAST.lu(G - N_ϵ)
+
+    MtE_map = - (G_N_ϵ_inv * R_0 - G_N_ϵ_inv * G * sum_Π_inv_matrix)
+    return MtE_map
+end
+
+## The Electric-to-Magnetic (EtM) OSRC operator is also implemented.
+# More details OSRC operator are given in the paper <a href="https://www.researchgate.net/publication/261636307_Approximate_local_magnetic-to-electric_surface_operators_for_time-harmonic_Maxwell's_equations" target='new'> Approximate local magnetic-to-electric surface operators for time-harmonic Maxwell’s equations (C. Geuzaine et al. (2014))</a>.
+
+struct EtM_OSRC_op
+    wavenumber::Float64
+    Np::Int
+    θ_p::Float64
+    curvature::Float64
+    r0::ComplexF64
+    rj::Vector{ComplexF64}
+    qj::Vector{ComplexF64}
+end
+
+# construct inverse pade rational
+
+function EtM_OSRC_op(wavenumber::Float64, Np::Int, θ_p::Float64, curvature::Float64)
+    # construct MtE operator with regular rotational branch cut Padé approximation
+    MtE_OSRC_op = OSRC_op(wavenumber, Np, θ_p, curvature)
+    pade_inv_num = get_denumerator(MtE_OSRC_op)
+    pade_inv_denum = get_numerator(MtE_OSRC_op)
+    qj = Polynomials.roots(pade_inv_denum)
+
+    # polynmial division to get constant term r0
+    r0, remainder = Polynomials.divrem(pade_inv_num, pade_inv_denum)  
+    P_deriv = Polynomials.derivative(pade_inv_denum)
+    residues = [remainder(p)/P_deriv(p) for p in qj] # compute residues=poles at every point
+    rj = residues
+    return EtM_OSRC_op(wavenumber, Np, θ_p, curvature, r0, rj, qj)
+end
+
+function rational_pade_inv(z, op::EtM_OSRC_op)
+    s = op.r0
+    for (r, q) in zip(op.rj, op.qj)
+        s += r / (z - q)
+    end
+    return s
+end
+
+
+function  assemble(op::EtM_OSRC_op,X::Space,Y::Space; quadstrat=defaultquadstrat)
+    ϵ = BEAST.MtE_damping(op)
+    κ = op.wavenumber
+    κ_ϵ = κ + imag_conv*ϵ
+
+    # assemble M2 matrix
+
+    curl_X = BEAST.curl(X);
+    curl_Y = BEAST.curl(Y);
+    P1 = BEAST.lagrangec0d1(X.geo);
+    P1_grad = BEAST.gradient(P1);
+
+    N_X = numfunctions(X)
+    N_Y = numfunctions(Y)
+
+    Id = BEAST.Identity();
+    A = assemble(Id, X, Y);
+    N = (1/κ_ϵ)^2 * assemble(Id, curl_X, curl_Y);
+    K = κ_ϵ^2 * assemble(Id, P1, P1);
+    L = assemble(Id, X, P1_grad);
+    L_transpose = assemble(Id, P1_grad, Y)
+
+    function get_M2(j)
+        Bq = - (op.qj[j]*A + N);
+        M2 = [Bq L
+              L_transpose K];
+        M2_inv = BEAST.lu(M2)
+        SliceMap = BEAST.SlicedLinearMap(M2_inv, 1:N_X, 1:N_Y)
+        P = SliceMap.P
+        Q = SliceMap.Q
+        M2_inv_sliced = P*M2_inv*Q
+        return M2_inv_sliced
+    end
+
+    A_inv = BEAST.lu(A)
+    EtM_map = A_inv * (op.r0*I + A * sum(op.rj[j]*get_M2(j) for j in 1:op.Np))*(A - N)
+    return EtM_map
+end
+
+function scalartype(op::EtM_OSRC_op)
+    T = scalartype(op.wavenumber)
+    return Complex{T}
+end
+
+
+##### Old/deprecated functions #####
 
 function MtE_operator(Γ, κ, Np::Int, theta_p::Float64; curvature = 1)
     pade_struct = Pade_approx(Np, theta_p);     # load in pade struct for pade coefficients and constants
@@ -262,55 +391,6 @@ function MtE_operator_GMRES(Γ, κ, Np::Int, theta_p::Float64; curvature = 1)
       return MtE_map
 end
 
-function assemble(op::OSRC_op,X::Space,Y::Space; quadstrat=defaultquadstrat)
-    R_0 = get_R0(op)
-
-    ϵ = MtE_damping(op)
-    κ = op.wavenumber
-    κ_ϵ = κ + imag_conv*ϵ
-
-    #create auxilary basis functions
-    L0_int = BEAST.lagrangec0d1(X.geo)
-    grad_L0_int = BEAST.gradient(L0_int)
-    # Define the relevant function spaces
-    curl_X = BEAST.curl(X)
-    curl_Y = BEAST.curl(Y)
-
-    N_L0 = numfunctions(L0_int)
-    N_X = numfunctions(X)
-    N_Y = numfunctions(Y)
-
-    # Assemble the submatrices of the blockmatrix of the system
-    Id = BEAST.Identity();
-    G = assemble(Id, X, Y)
-    N_ϵ = (1/κ_ϵ)^2 * assemble(Id, curl_X, curl_Y)
-    K_ϵ = κ_ϵ^2 * assemble(Id, L0_int, L0_int)
-    L = assemble(Id, X, grad_L0_int)
-    L_transpose = assemble(Id, grad_L0_int, Y)
-
-    # construct the sparse system matrix and invert
-    function create_j_phi_matrix17(j)
-        B_j = op.Bj[j]
-        # blockmatrix of sparse matrices
-        AXY = [G-B_j*N_ϵ       B_j*L
-                L_transpose     K_ϵ]
-        SXY = BEAST.lu(AXY)
-        Sliced_SXY = SlicedLinearMap(SXY, 1:N_X, 1:N_Y)
-        P = Sliced_SXY.P
-        Q = Sliced_SXY.Q
-        SXY_sliced = P*SXY*Q
-        return SXY_sliced
-    end
-
-    sum_Π_inv_matrix = sum(op.Aj[j]/op.Bj[j] * create_j_phi_matrix17(j) for j in 1:op.Np)
-    G_N_ϵ_inv = BEAST.lu(G - N_ϵ)
-
-    MtE_map = - (G_N_ϵ_inv * R_0 - G_N_ϵ_inv * G * sum_Π_inv_matrix)
-    return MtE_map
-end
-
-
-
 function Cheap_OSRC_preconditioner(Γ, κ; curvature = 1)
     ϵ = MtE_damping(wavenumber=κ, curvature=curvature)
     κ_ϵ = κ + imag_conv*ϵ
@@ -324,86 +404,4 @@ function Cheap_OSRC_preconditioner(Γ, κ; curvature = 1)
     N_ϵ = sparse(assemble((1/κ_ϵ)^2 * Id, curl_Nd, curl_Nd))
     G_N_ϵ_inv = BEAST.lu(G - N_ϵ)
     return G_N_ϵ_inv
-end
-
-# More details of this OSRC operator are given in the paper <a href="https://www.researchgate.net/publication/261636307_Approximate_local_magnetic-to-electric_surface_operators_for_time-harmonic_Maxwell's_equations" target='new'> Approximate local magnetic-to-electric surface operators for time-harmonic Maxwell’s equations (C. Geuzaine et al. (2014))</a>.
-
-struct EtM_OSRC_op
-    wavenumber::Float64
-    Np::Int
-    θ_p::Float64
-    curvature::Float64
-    r0::ComplexF64
-    rj::Vector{ComplexF64}
-    qj::Vector{ComplexF64}
-end
-
-# construct inverse pade rational
-
-function EtM_OSRC_op(wavenumber::Float64, Np::Int, θ_p::Float64, curvature::Float64)
-    # construct MtE operator with regular rotational branch cut Padé approximation
-    MtE_OSRC_op = OSRC_op(wavenumber, Np, θ_p, curvature)
-    pade_inv_num = get_denumerator(MtE_OSRC_op)
-    pade_inv_denum = get_numerator(MtE_OSRC_op)
-    qj = Polynomials.roots(pade_inv_denum)
-
-    # polynmial division to get constant term r0
-    r0, remainder = Polynomials.divrem(pade_inv_num, pade_inv_denum)  
-    P_deriv = Polynomials.derivative(pade_inv_denum)
-    residues = [remainder(p)/P_deriv(p) for p in qj] # compute residues=poles at every point
-    rj = residues
-    return EtM_OSRC_op(wavenumber, Np, θ_p, curvature, r0, rj, qj)
-end
-
-function rational_pade_inv(z, op::EtM_OSRC_op)
-    s = op.r0
-    for (r, q) in zip(op.rj, op.qj)
-        s += r / (z - q)
-    end
-    return s
-end
-
-
-function  assemble(op::EtM_OSRC_op,X::Space,Y::Space; quadstrat=defaultquadstrat)
-    ϵ = BEAST.MtE_damping(op)
-    κ = op.wavenumber
-    κ_ϵ = κ + imag_conv*ϵ
-
-    # assemble M2 matrix
-
-    curl_X = BEAST.curl(X);
-    curl_Y = BEAST.curl(Y);
-    P1 = BEAST.lagrangec0d1(X.geo);
-    P1_grad = BEAST.gradient(P1);
-
-    N_X = numfunctions(X)
-    N_Y = numfunctions(Y)
-
-    Id = BEAST.Identity();
-    A = assemble(Id, X, Y);
-    N = (1/κ_ϵ)^2 * assemble(Id, curl_X, curl_Y);
-    K = κ_ϵ^2 * assemble(Id, P1, P1);
-    L = assemble(Id, X, P1_grad);
-    L_transpose = assemble(Id, P1_grad, Y)
-
-    function get_M2(j)
-        Bq = - (op.qj[j]*A + N);
-        M2 = [Bq L
-              L_transpose K];
-        M2_inv = BEAST.lu(M2)
-        SliceMap = BEAST.SlicedLinearMap(M2_inv, 1:N_X, 1:N_Y)
-        P = SliceMap.P
-        Q = SliceMap.Q
-        M2_inv_sliced = P*M2_inv*Q
-        return M2_inv_sliced
-    end
-
-    A_inv = BEAST.lu(A)
-    EtM_map = A_inv * (op.r0*I + A * sum(op.rj[j]*get_M2(j) for j in 1:op.Np))*(A - N)
-    return EtM_map
-end
-
-function scalartype(op::EtM_OSRC_op)
-    T = scalartype(op.wavenumber)
-    return Complex{T}
 end
